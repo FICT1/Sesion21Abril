8365525ec07034653a82575569b899fc4055e7bb
FICT1 <isaquitopro10@gmail.com>
2026-04-22 15:42:21 -0600

    Initial commit

---
+++ src/main/java/ni/edu/uam/servicios/ProductoServicio.java
@@ -0,0 +1,38 @@
+package ni.edu.uam.servicios;
+
+import ni.edu.uam.interfaces.ProductoInterfaces;
+import ni.edu.uam.modelos.Producto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProductoServicio implements ProductoInterfaces {
+
+    private List<Producto> productos;
+
+    public ProductoServicio() {
+        this.productos = new ArrayList<>();
+    }
+
+
+    @Override
+    public void agregarProducto(String nombre, double precio, int cantidad) {
+        this.productos.add(new Producto(nombre, cantidad, precio));
+
+    }
+
+    @Override
+    public List<Producto> getProductos() {
+
+        return productos;
+    }
+
+    public double getMonto(){
+        double total = 0, monto = 0;
+        for(Producto producto : productos){
+            total = producto.getCantidad() * producto.getPrecio();
+            monto += total;
+        }
+        return monto;
+    }
+}
