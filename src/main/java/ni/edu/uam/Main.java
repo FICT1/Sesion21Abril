8407292403611cc211cfeea77501be60de60ceb1
FICT1 <isaquitopro10@gmail.com>
2026-04-22 16:36:47 -0600

    Cambios nuevos (hechos por el profe)

--- src/main/java/ni/edu/uam/Main.java
+++ src/main/java/ni/edu/uam/Main.java
@@ -3,22 +3,47 @@ package ni.edu.uam;
 import ni.edu.uam.modelos.Producto;
 import ni.edu.uam.servicios.ProductoServicio;
 
+import javax.swing.*;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+//hacer que los datos se soliciten
+//hacer un menú
+//con JOptionPane
+
 
 public class Main {
     static void main() {
         ProductoServicio productos = new ProductoServicio();
 
+        productos.agregarProducto("Coca-cola", 35, 2);
+        productos.agregarProducto("Cuajada", 30, 2);
+        productos.agregarProducto("Pan", 10, 3);
+
+        String menu = "Jaguar Coffee \n1. Agregar producto \n2. Ver productos \n3. Salir \nOpcion: ";
+        String nombre, precio, cantidad, opcion = "0";
+        do {
+            opcion = JOptionPane.showInputDialog(null, menu);
+            switch (opcion) {
+                case "1" ->{
+                        nombre = JOptionPane.showInputDialog(null, "Ingrese el nombre del producto: ");
+                        precio = JOptionPane.showInputDialog(null, "Ingrese el precio del producto: ");
+                        cantidad = JOptionPane.showInputDialog(null, "Ingrese la cantidad del producto: ");
+                        productos.agregarProducto(nombre, Double.parseDouble(precio), Integer.parseInt(cantidad));
+                }
+                case "2" -> {
+                    JOptionPane.showMessageDialog(null, productos.getFactura());
 
 
-        productos.agregarProducto("Café", 40, 3);
-        productos.agregarProducto("Tajadas con queso", 60, 3);
+                }
 
-        System.out.println("Factura");
-        System.out.println(productos.getProductos());
-        System.out.println("Total a pagar: "+ productos.getMonto());;
+                case "3" ->{
+                    JOptionPane.showMessageDialog(null, "Gracias por su compra");
+                }
 
+                default -> JOptionPane.showMessageDialog(null, "Opcion no valida");
+            }
+
+        }while (!opcion.equals("3")) ;
     }
 }
